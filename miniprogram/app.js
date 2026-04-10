65b7f5ae05599af51d1125aa94c2ad4aca6b8aa7
simoneqing <simoneqing@gmail.com>
2026-04-10 20:34:11 +0800

    fix(miniprogram-history): keep records visible with cloud-empty fallback cache

---
+++ miniprogram/app.js
@@ -0,0 +1,13 @@
+App({
+  onLaunch() {
+    if (!wx.cloud) {
+      console.error('请使用支持云开发的基础库版本');
+      return;
+    }
+
+    wx.cloud.init({
+      env: 'cloud1-4gp46try5b214',
+      traceUser: true
+    });
+  }
+});
